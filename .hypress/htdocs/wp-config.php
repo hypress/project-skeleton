dd86290cba41f097b99b88cd0d5b26c6940bed7c
Cem Derin <cem@wysiwyg.de>
2019-06-06 21:34:02 +0200

    Docker implementation WIP

--- .hypress/htdocs/wp-config.php
+++ .hypress/htdocs/wp-config.php
@@ -24,13 +24,13 @@
 	define('DB_NAME', 'hypress');
 	
 	/** MySQL database username */
-	define('DB_USER', 'hypress');
+	define('DB_USER', 'root');
 	
 	/** MySQL database password */
 	define('DB_PASSWORD', 'hypress');
 	
 	/** MySQL hostname */
-	define('DB_HOST', 'localhost');
+	define('DB_HOST', 'hypress_mysql');
 	
 	/** Database Charset to use in creating database tables. */
 	define('DB_CHARSET', 'utf8');
